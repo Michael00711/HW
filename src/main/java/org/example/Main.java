94437cb78023c6751322721183749857fccea5e5
Michael <ananevr4@gmail.com>
2024-05-08 16:08:14 +0300

    hw

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        System.out.println(x + y);
+        System.out.print(x);
+        System.out.println(y);
+        int i = 5;
+        i = i ++;
+        System.out.println(i);
+        i = 5;
+        i = i++ + i++;
+        System.out.println(i);
+        i = 5;
+        i = i++ + ++i;
+        System.out.println(i);
+        i = 5;
+        i = ++i + ++i;
+        System.out.println(i);
+
+
+    }
+    }
+
